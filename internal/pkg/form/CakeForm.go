76e76c15561ba6536162eef26efcb74fb012f70f
Bayu Pratama <bayu.pratama@sadhana-corp.id>
2025-07-23 10:55:42 +0800

    add form

--- internal/pkg/form/CakeForm.go
+++ internal/pkg/form/CakeForm.go
@@ -8,30 +8,30 @@ import (
 )
 
 type CakeForm struct {
-	Name        string  `json:"name" validate:"required,max=250"`
-	Description *string `json:"description"`
-	Margin      float64 `json:"margin" validate:"required,gte=0"`
-	UnitId      int     `json:"unitId" validate:"required"`
-	Stock       int     `json:"stock" validate:"gte=0"`
+	Name        string  `json:"name" form:"name" validate:"required,max=250"`
+	Description *string `json:"description" form:"description"`
+	Margin      float64 `json:"margin" form:"margin" validate:"required,gte=0"`
+	UnitId      int     `json:"unitId" form:"unitId" validate:"required"`
+	Stock       int     `json:"stock" form:"stock" validate:"gte=0"`
 
-	Ingredients []CakeIngredientForm `json:"ingredients" validate:"required,dive"`
-	Costs       []CakeCostForm       `json:"costs" validate:"required,dive"`
+	Ingredients []CakeIngredientForm `json:"ingredients" form:"ingredients" validate:"required,dive"`
+	Costs       []CakeCostForm       `json:"costs" form:"costs" validate:"required,dive"`
 	Request     *http.Request
 }
 
 type CakeIngredientForm struct {
-	ID           uint    `json:"id"`
-	IngredientId uint    `json:"ingredientId" validate:"required_without=ID"`
-	Amount       float64 `json:"amount" validate:"required_without=ID"`
-	UnitId       int     `json:"unitId" validate:"required_without=ID"`
-	Deleted      bool    `json:"deleted"`
+	ID           uint    `json:"id" form:"id"`
+	IngredientId uint    `json:"ingredientId" form:"ingredientId" validate:"required_without=ID"`
+	Amount       float64 `json:"amount" form:"amount" validate:"required_without=ID"`
+	UnitId       int     `json:"unitId" form:"unitId" validate:"required_without=ID"`
+	Deleted      bool    `json:"deleted" form:"deleted"`
 }
 
 type CakeCostForm struct {
-	ID      uint    `json:"id"`
-	TypeId  int     `json:"typeId" validate:"required_without=ID"`
-	Price   float64 `json:"price" validate:"required_without=ID"`
-	Deleted bool    `json:"deleted"`
+	ID      uint    `json:"id" form:"id"`
+	TypeId  int     `json:"typeId" form:"typeId" validate:"required_without=ID"`
+	Price   float64 `json:"price" form:"price" validate:"required_without=ID"`
+	Deleted bool    `json:"deleted" form:"deleted"`
 }
 
 func (f *CakeForm) Validate() {
